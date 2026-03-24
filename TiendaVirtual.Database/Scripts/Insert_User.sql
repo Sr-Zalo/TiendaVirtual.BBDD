27fb9c7c8aca4dee086c6b473b0c59be196afb7b
Sr-Zalo <gonlolu04@gmail.com>
2026-03-24 20:00:26 +0100

    feat: actualizacion de contraseña

--- TiendaVirtual.Database/Scripts/Insert_User.sql
+++ TiendaVirtual.Database/Scripts/Insert_User.sql
@@ -3,7 +3,7 @@ SET IDENTITY_INSERT [dbo].[User] ON
 GO
 IF NOT EXISTS (SELECT 1 FROM [dbo].[User] WHERE [UserId] = 1)
     INSERT INTO [dbo].[User] ([UserId], [RoleId], [Name], [Surname], [Email], [PasswordHash], [Enabled], [IUser], [IDate], [IComments])
-    VALUES (1, 1, 'Admin', 'Admin', 'admin@tienda.com', 'AQAAAAIAAYagAAAAEDjzCHdHxvbJCNdKZb1VKZFXlvamrUj6jbpaCQIRnrixMRLzC+Hekl280HMLXO8sgg==', 1, '1', GETDATE(), 'Carga inicial')
+    VALUES (1, 1, 'Admin', 'Admin', 'admin@tienda.com', '$2a$11$Wf/.Tumq9YdLiO86pZIPduTDlwOu3oloVdWEpVX44HpsLKGN6WQou', 1, '1', GETDATE(), 'Carga inicial')
 GO
 SET IDENTITY_INSERT [dbo].[User] OFF
 GO
